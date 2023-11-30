92bdf4d8d1e7d23befeff1d0e9af9a6e7ee83d75
SandlerJr <sandlerjunin@gmail.com>
2023-11-30 11:25:07 -0300

    Separando conteúdo

--- Aula1/objetos.js
+++ Aula1/objetos.js
@@ -7,7 +7,3 @@ const cliente = {
   
 //Acessando o valor do objeto
 console.log(cliente);
-
-//Manipulando valor do objeto
-console.log(`O nome do cliente é ${cliente.nome} e essa pessoa tem ${cliente.idade} anos.`);
-console.log(`Os 3 primeiros digitos do CPF são "${cliente.cpf.substring(0, 3)}********" `)

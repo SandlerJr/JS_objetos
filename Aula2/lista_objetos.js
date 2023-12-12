5d4c4ee8c73d061d908cadf92fe27df0a0f2a1f8
SandlerJr <sandlerjunin@gmail.com>
2023-12-12 07:47:02 -0300

    Criando uma lista de endereços, com endereço sendo um objeto

---
+++ Aula2/lista_objetos.js
@@ -0,0 +1,26 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"],
+};
+
+cliente.enderecos = [
+    {
+        rua: "R. Joseph Climber",
+        numero: 1337,
+        apartamento: true,
+        complemento: "apto. 934",
+    }
+]
+
+cliente.enderecos.push(
+    {
+        rua: "R. Joseph Ladder",
+        numero: 337,
+        apartamento: false,
+    }
+)
+
+console.log(cliente.enderecos);
+

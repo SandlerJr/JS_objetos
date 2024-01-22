1234d4631dd185f114bef79fc2697fde6ffb27e9
SandlerJr <sandlerjunin@gmail.com>
2024-01-22 19:40:42 -0300

    Transformando o objeto JSON em uma String e adicionando a uma nova const

--- Aula4/leitura.js
+++ Aula4/leitura.js
@@ -2,3 +2,7 @@ const dados = require('./cliente.json');
 
 console.log(dados);
 console.log('O tipo de dado desse arquivo é: ' + typeof dados);
+
+const clienteEmString = JSON.stringify(dados);
+console.log(dados);
+console.log('O novo tipo de arquivo é: ' + typeof clienteEmString);
